b0b01ec43f6fd437d890017e2fe16e4097f00755
Joshua Durana <rjdurana@gmail.com>
2021-02-28 00:41:35 -0600

    added location method

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const axios = require('axios')
 require('dotenv').config()
+app.use(express.json())
 
 app.get('/test', function (req, res) {
     var config = {
@@ -17,11 +18,33 @@ app.get('/test', function (req, res) {
 
     axios(config)
     .then(function (response) {
-        console.log(JSON.stringify(response.data));
         res.status(200).json(response.data);
       })
       .catch(function (error) {
         console.log(error);
+        res.status(400).json({error:"An error has occurred"})
+      });
+})
+
+app.get('/location', (req, res)=>{
+  let zipCode = req.query.zipCode;
+  
+  var config = {
+    method: 'get',
+    url: 'https://api.kroger.com/v1/locations/?filter.zipCode.near=' + zipCode,
+    headers: {
+      'Authorization': 'Bearer ' + process.env.KEY,
+      'Cookie': process.env.COOKIE
+    }
+  };
+
+  axios(config)
+    .then(function (response) {
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        res.status(400).json({error:"An error has occurred"})
       });
 })
 
