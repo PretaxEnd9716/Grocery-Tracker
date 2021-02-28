56f72a0ae93eef07744cb4952f8b8f2162d92f7e
Joshua Durana <rjdurana@gmail.com>
2021-02-27 23:37:46 -0600

    Working API Call

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const app = express()
+require('dotenv').config();
 
 const axios = require('axios')
 require('dotenv').config()
 
 app.get('/test', function (req, res) {
-    const key = 'Bearer ' + process.env.API_KEY
-    const headers = {headers:{
-        'Accept': 'application/json',
-        'Authorization': key
-    }}
-    axios.get('https://api.kroger.com/v1/products?filter.term=milk&filter.limit=2', headers)
-    .then(function(response) {
-        console.log(response.data)
+    var config = {
+        method: 'get',
+        url: 'https://api.kroger.com/v1/locations/?filter.zipCode.near=77494',
+        headers: { 
+          'Authorization': 'Bearer ' + process.env.KEY, 
+          'Cookie': process.env.COOKIE
+        }
+      };
+
+    axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
         res.status(200).json(response.data);
-    })
-    .catch(function(error) {
-        console.log(error)
-        res.status(400).json({error:"An error occured"})
-    })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 })
 
 app.listen(3000, () => console.log(`Example app listening at http://localhost:3000`))
